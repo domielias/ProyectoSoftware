5049507418b8e6e245160930e4e67e24177d560d
Elias Rodriguez <45079270+domielias@users.noreply.github.com>
2020-02-19 04:23:58 +0100

    Add update

--- db/migrate/20200219023710_create_personas.rb
+++ db/migrate/20200219023710_create_personas.rb
@@ -2,7 +2,6 @@ class CreatePersonas < ActiveRecord::Migration[6.0]
   def change
     create_table :personas, id: :uuid do |t|
       t.has_one :pai,
-      t.has_one :pai, dependent: :
       t.string :nombres, limit: 50
       t.string :apellidos, limit: 50
       t.date :fecha_nacimiento
