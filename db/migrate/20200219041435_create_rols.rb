c3c54f844af8079171873ba3dd306c336ddd3dca
Elias Rodriguez <45079270+domielias@users.noreply.github.com>
2020-02-22 21:48:10 +0100

    change uuid in rol

--- db/migrate/20200219041435_create_rols.rb
+++ db/migrate/20200219041435_create_rols.rb
@@ -1,6 +1,6 @@
 class CreateRols < ActiveRecord::Migration[6.0]
   def change
-    create_table :rols, id: :uuid do |t|
+    create_table :rols do |t|
       t.string :nombre, limit: 20
 
       t.references :usuario, foreign_key :true
