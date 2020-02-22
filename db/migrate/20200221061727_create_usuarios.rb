085a23d609d8f44f6bd03f20bfc81ce8869024df
Elias Rodriguez <45079270+domielias@users.noreply.github.com>
2020-02-22 22:18:19 +0100

    delete uuid from user

--- db/migrate/20200221061727_create_usuarios.rb
+++ db/migrate/20200221061727_create_usuarios.rb
@@ -1,6 +1,6 @@
 class CreateUsuarios < ActiveRecord::Migration[6.0]
   def change
-    create_table :usuarios, id: :uuid do |t|
+    create_table :usuarios do |t|
       t.string :nombre, limit: 50
       t.string :password, limit: 50
 
