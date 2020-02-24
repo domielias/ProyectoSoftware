8fb4736fd8a61d8af01e2e7b9303168e9497132b
hardow2011 <hardow2011@outlook.com>
2020-02-24 15:38:49 -0400

    Formulario de estudiante empezado

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,2 +1,5 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
+//= link_tree ../fonts
+//= link_tree ../javascript
+//= link_tree ../stylesheets
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,12 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-import "bootstrap"
+require("bootstrap")
+
+// stylesheets
+require("../stylesheets/main.scss")
+require("../vendor/*")
+
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
