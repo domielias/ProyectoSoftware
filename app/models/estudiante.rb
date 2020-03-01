b12f2f91f4c1555ee6acb885dbdc405fc0bef1a0
hardow2011 <hardow2011@outlook.com>
2020-02-29 23:11:27 -0400

    Avences creacion estud

--- app/models/estudiante.rb
+++ app/models/estudiante.rb
@@ -1,7 +1,7 @@
 class Estudiante < ApplicationRecord
-    belongs_to :facultad
-    belongs_to :programa_internacional
-    belongs_to :carrera_solicitada
+    belongs_to :facultad, optional: true
+    belongs_to :programa_internacional, optional: true
+    belongs_to :carrera_solicitada, optional: true
     has_one :padre, :class_name => 'Persona'
     has_one :madre, :class_name => 'Persona'
     has_many :direccions
@@ -12,17 +12,17 @@ class Estudiante < ApplicationRecord
     has_and_belongs_to_many :clases
     belongs_to :persona
 
-    accepts_nested_attributes_for :facultad
-    accepts_nested_attributes_for :programa_internacional
-    accepts_nested_attributes_for :carrera_solicitada
-    accepts_nested_attributes_for :padre
-    accepts_nested_attributes_for :madre
-    accepts_nested_attributes_for :direccions
-    accepts_nested_attributes_for :examen_de_nivel
-    accepts_nested_attributes_for :informacion_academica
-    accepts_nested_attributes_for :progreso_inscripcion
-    accepts_nested_attributes_for :bloques
-    accepts_nested_attributes_for :clases
+    # accepts_nested_attributes_for :facultad
+    # accepts_nested_attributes_for :programa_internacional
+    # accepts_nested_attributes_for :carrera_solicitada
+    # accepts_nested_attributes_for :padre
+    # accepts_nested_attributes_for :madre
+    # accepts_nested_attributes_for :direccions
+    # accepts_nested_attributes_for :examen_de_nivel
+    # accepts_nested_attributes_for :informacion_academica
+    # accepts_nested_attributes_for :progreso_inscripcion
+    # accepts_nested_attributes_for :bloques
+    # accepts_nested_attributes_for :clases
     accepts_nested_attributes_for :persona
 
 end
