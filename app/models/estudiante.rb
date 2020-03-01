f88177afcbf28bf19fb343a952085b8a6966c6a9
hardow2011 <hardow2011@outlook.com>
2020-03-01 04:28:39 -0400

    Intento opcion lugar nacimiento

--- app/models/estudiante.rb
+++ app/models/estudiante.rb
@@ -1,17 +1,18 @@
 class Estudiante < ApplicationRecord
 
-    belongs_to :facultad
-    belongs_to :programa_internacional
-    belongs_to :carrera_solicitada
-    belongs_to :padre, :class_name => 'Persona'
-    belongs_to :madre, :class_name => 'Persona'
+    belongs_to :facultad, optional: true
+    belongs_to :programa_internacional, optional: true
+    belongs_to :carrera_solicitada, optional: true
+    belongs_to :padre, :class_name => 'Persona', optional: true
+    belongs_to :madre, :class_name => 'Persona', optional: true
     has_many :direccions
     has_one :examen_de_nivel
     has_one :informacion_academica
     has_one :progreso_inscripcion
     has_and_belongs_to_many :bloques
     has_and_belongs_to_many :clases
-    belongs_to :persona
+    belongs_to :persona, optional: true
+    has_many :persona_pais, through: :persona
 
     accepts_nested_attributes_for :facultad
     accepts_nested_attributes_for :programa_internacional
@@ -22,6 +23,7 @@ class Estudiante < ApplicationRecord
     accepts_nested_attributes_for :examen_de_nivel
     accepts_nested_attributes_for :informacion_academica
     accepts_nested_attributes_for :progreso_inscripcion
+    accepts_nested_attributes_for :persona_pais
     # accepts_nested_attributes_for :bloques
     # accepts_nested_attributes_for :clases
     accepts_nested_attributes_for :persona
