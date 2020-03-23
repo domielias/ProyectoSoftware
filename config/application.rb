b129e2819b02bc38be7d4ee4916db433fbce639e
Elias Rodriguez <45079270+domielias@users.noreply.github.com>
2020-03-23 13:21:51 -0400

    fixing heroku error

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module Pro
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     config.assets.enabled = true
+    config.assets.initialize_on_precompile = false
+
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
