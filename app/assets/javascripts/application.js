cdf9ecaf693e5500dd59882fde24bdc085425523
Elias Rodriguez <45079270+domielias@users.noreply.github.com>
2020-03-23 11:54:23 -0400

    fixing heroku error

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,3 +6,35 @@
 //= require select2_locale_es
 //= require bootstrap-datepicker
 //= require_tree .
+
+$(document).on('turbolinks:load', function () {
+  $('.select2-simple-dropdown').select2({placeholder: "Elegir", allowClear: true, width: '100%'});
+});
+$(document).on('turbolinks:load', function () {
+  $('.select2-multiple-dropdown').select2({allowClear: true, placeholder: "Elegir 1 o más", closeOnSelect: false, width: '100%'});
+
+});
+
+$(document).ready(function () {
+  $('.datepicker').datepicker({format: 'dd/mm/yyyy'});
+});
+
+$(document).ready(function () {
+  $('#myTable').DataTable();
+});
+
+$(document).on('turbolinks:load', function() {
+  $("table[role='datatable']").each(function() {
+    $(this).DataTable({
+      "destroy": true,
+      "ordering": false,
+      "language": {
+        "search": "Buscar:"
+      },
+      "paging": false,
+      "pageLength": 10,
+      "pagingType": "full_numbers"
+    });
+    $('input').addClass('form-control');
+  });
+})
