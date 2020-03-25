3ce5821ec8db11460c272c4bbf44630807a0670c
hardow2011 <hardow2011@outlook.com>
2020-03-25 00:02:22 -0400

    config.assets.initialize_on_precompile = false

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module Pro
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     config.assets.enabled = true
+    config.assets.initialize_on_precompile = false
 
 
     # Settings in config/environments/* take precedence over those specified here.
