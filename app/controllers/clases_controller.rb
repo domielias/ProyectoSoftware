aa040e7ef406f8bc5a21993533609e5ba2240a8f
hardow2011 <hardow2011@outlook.com>
2020-04-20 02:40:52 -0400

    Vista listado de clases del profe completada

--- app/controllers/clases_controller.rb
+++ app/controllers/clases_controller.rb
@@ -1,14 +1,16 @@
 class ClasesController < ApplicationController
+  load_and_authorize_resource
   before_action :set_clase, only: [:show, :edit, :update, :destroy]
   # GET /clases
   # GET /clases.json
   def index
-    @clases = Clase.all
+    @clases = Clase.accessible_by(current_ability)
   end
 
   # GET /clases/1
   # GET /clases/1.json
   def show
+
   end
 
   # GET /clases/new
@@ -32,46 +34,24 @@ class ClasesController < ApplicationController
   def create
     @clase = Clase.new(clase_params)
 
-    params[:estudiantes][:id].each do |estudiante|
-      if !estudiante.empty?
-        @clase.clase_estudiantes.build(estudiante_id: estudiante)
-      end
+    if @clase.save
+      redirect_to clases_url
+    else
+      format.html { render :new }
     end
 
-    respond_to do |format|
-      if @clase.save
-        format.html { redirect_to @clase, notice: 'Clase was successfully created.' }
-        format.json { render :show, status: :created, location: @clase }
-      else
-        format.html { render :new }
-        format.json { render json: @clase.errors, status: :unprocessable_entity }
-      end
-    end
   end
 
   # PATCH/PUT /clases/1
   # PATCH/PUT /clases/1.json
   def update
 
-    ClaseEstudiante.where(clase_id: @clase).where.not(estudiante_id: params[:estudiantes][:id].reject { |e| e.empty? }).delete_all
-
-    params[:estudiantes][:id].each do |estudiante|
-      if !estudiante.empty?
-        if !ClaseEstudiante.exists?(clase_id: @clase, estudiante_id: estudiante)
-        @clase.clase_estudiantes.build(estudiante_id: estudiante)
-        end
-      end
+    if @clase.update(clase_params)
+      redirect_to clases_url
+    else
+      format.html { render :new }
     end
 
-    respond_to do |format|
-      if @clase.update(clase_params)
-        format.html { redirect_to @clase, notice: 'Clase was successfully updated.' }
-        format.json { render :show, status: :ok, location: @clase }
-      else
-        format.html { render :edit }
-        format.json { render json: @clase.errors, status: :unprocessable_entity }
-      end
-    end
   end
 
   # DELETE /clases/1
@@ -92,6 +72,6 @@ class ClasesController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def clase_params
-    params.require(:clase).permit(:fecha_inicio, :fecha_final, :seccion, :activo, :no_clase, :lugar, :modalidad, :correquisito, :ciclo_id, :profesor_id, :asignatura_id, :nivel_id, estudiante_ids: [], horarios_attributes: [:id, :dias, :start, :end, :_destroy])
+    params.require(:clase).permit(:seccion, :no_clase, :lugar, :modalidad, :correquisito, :asignatura_id, :profesor_id, estudiante_ids: [], horarios_attributes: [:id, :dias, :start, :end, :_destroy])
   end
 end
