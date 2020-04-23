ec55e409fc562110261a0f4a7701319ded93dfef
Elias Rodriguez <45079270+domielias@users.noreply.github.com>
2020-04-23 07:39:00 -0400

    uploading migrating for bloque

--- db/migrate/33_create_bloque_estudiantes.rb
+++ db/migrate/33_create_bloque_estudiantes.rb
@@ -3,6 +3,7 @@ class CreateBloqueEstudiantes < ActiveRecord::Migration[6.0]
     create_table :bloque_estudiantes do |t|
       t.integer :bloque_id, null: false
       t.integer :estudiante_id, null: false
+      t.string :asignaturas_solicitadas, limit: 100
 
       t.timestamps
     end
