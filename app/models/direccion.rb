068f6727bf458a9930e072023a9db789f389020e
hardow2011 <hardow2011@outlook.com>
2020-06-12 10:33:45 -0400

    Modelo de direccion modificado para que ya no cambien de orden en el editar de estudiante.

--- app/models/direccion.rb
+++ app/models/direccion.rb
@@ -1,4 +1,6 @@
 class Direccion < ApplicationRecord
     belongs_to :estudiante
+    # Las direcciones serán ordenadas por id en el form de editar estudiante
+    default_scope { order(:id) }
     belongs_to :pai, optional: true
 end
