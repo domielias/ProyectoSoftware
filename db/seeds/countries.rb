9861457ba69f7e9dcf2947daeec18457d598ed7d
Eliass Rodriguezz <elflautista321@gmail.com>
2021-07-11 11:50:22 -0400

    añadiendo registro de paises automaticos a la base de datos.

---
+++ db/seeds/countries.rb
@@ -0,0 +1,988 @@
+#Insert all countries in the Pai Table
+Pai.create([
+  {
+  "nombre": " Afghanistan",
+  "nacionalidad": "Afghan"
+  },
+  {
+  "nombre": " Albania",
+  "nacionalidad": "Albanian"
+  },
+  {
+  "nombre": " Aland Islands",
+  "nacionalidad": "Aland Islander"
+  },
+  {
+  "nombre": " Algeria",
+  "nacionalidad": "Algerian"
+  },
+  {
+  "nombre": " American Samoa",
+  "nacionalidad": "American Samoan"
+  },
+  {
+  "nombre": " Andorra",
+  "nacionalidad": "Andorran"
+  },
+  {
+  "nombre": " Angola",
+  "nacionalidad": "Angolan"
+  },
+  {
+  "nombre": " Anguilla",
+  "nacionalidad": "Anguillan"
+  },
+  {
+  "nombre": " Antarctica",
+  "nacionalidad": "Antarctican"
+  },
+  {
+  "nombre": " Antigua and Barbuda",
+  "nacionalidad": "Antiguan"
+  },
+  {
+  "nombre": " Argentina",
+  "nacionalidad": "Argentinian"
+  },
+  {
+  "nombre": " Armenia",
+  "nacionalidad": "Armenian"
+  },
+  {
+  "nombre": " Aruba",
+  "nacionalidad": "Aruban"
+  },
+  {
+  "nombre": " Australia",
+  "nacionalidad": "Australian"
+  },
+  {
+  "nombre": " Austria",
+  "nacionalidad": "Austrian"
+  },
+  {
+  "nombre": " Azerbaijan",
+  "nacionalidad": "Azerbaijani"
+  },
+  {
+  "nombre": " Bahamas",
+  "nacionalidad": "Bahamian"
+  },
+  {
+  "nombre": " Bahrain",
+  "nacionalidad": "Bahraini"
+  },
+  {
+  "nombre": " Bangladesh",
+  "nacionalidad": "Bangladeshi"
+  },
+  {
+  "nombre": " Barbados",
+  "nacionalidad": "Barbadian"
+  },
+  {
+  "nombre": " Belarus",
+  "nacionalidad": "Belarusian"
+  },
+  {
+  "nombre": " Belgium",
+  "nacionalidad": "Belgian"
+  },
+  {
+  "nombre": " Belize",
+  "nacionalidad": "Belizean"
+  },
+  {
+  "nombre": " Benin",
+  "nacionalidad": "Beninese"
+  },
+  {
+  "nombre": " Saint Barthelemy",
+  "nacionalidad": "Saint Barthelmian"
+  },
+  {
+  "nombre": " Bermuda",
+  "nacionalidad": "Bermudan"
+  },
+  {
+  "nombre": " Bhutan",
+  "nacionalidad": "Bhutanese"
+  },
+  {
+  "nombre": " Bolivia",
+  "nacionalidad": "Bolivian"
+  },
+  {
+  "nombre": " Bosnia and Herzegovina",
+  "nacionalidad": "Bosnian / Herzegovinian"
+  },
+  {
+  "nombre": " Botswana",
+  "nacionalidad": "Botswanan"
+  },
+  {
+  "nombre": " Bouvet Island",
+  "nacionalidad": "Bouvetian"
+  },
+  {
+  "nombre": " Brazil",
+  "nacionalidad": "Brazilian"
+  },
+  {
+  "nombre": " British Indian Ocean Territory",
+  "nacionalidad": "British Indian Ocean Territory"
+  },
+  {
+  "nombre": " Brunei Darussalam",
+  "nacionalidad": "Bruneian"
+  },
+  {
+  "nombre": " Bulgaria",
+  "nacionalidad": "Bulgarian"
+  },
+  {
+  "nombre": " Burkina Faso",
+  "nacionalidad": "Burkinabe"
+  },
+  {
+  "nombre": " Burundi",
+  "nacionalidad": "Burundian"
+  },
+  {
+  "nombre": " Cambodia",
+  "nacionalidad": "Cambodian"
+  },
+  {
+  "nombre": " Cameroon",
+  "nacionalidad": "Cameroonian"
+  },
+  {
+  "nombre": " Canada",
+  "nacionalidad": "Canadian"
+  },
+  {
+  "nombre": " Cape Verde",
+  "nacionalidad": "Cape Verdean"
+  },
+  {
+  "nombre": " Cayman Islands",
+  "nacionalidad": "Caymanian"
+  },
+  {
+  "nombre": " Central African Republic",
+  "nacionalidad": "Central African"
+  },
+  {
+  "nombre": " Chad",
+  "nacionalidad": "Chadian"
+  },
+  {
+  "nombre": " Chile",
+  "nacionalidad": "Chilean"
+  },
+  {
+  "nombre": " China",
+  "nacionalidad": "Chinese"
+  },
+  {
+  "nombre": " Christmas Island",
+  "nacionalidad": "Christmas Islander"
+  },
+  {
+  "nombre": " Cocos (Keeling) Islands",
+  "nacionalidad": "Cocos Islander"
+  },
+  {
+  "nombre": " Colombia",
+  "nacionalidad": "Colombian"
+  },
+  {
+  "nombre": " Comoros",
+  "nacionalidad": "Comorian"
+  },
+  {
+  "nombre": " Congo",
+  "nacionalidad": "Congolese"
+  },
+  {
+  "nombre": " Cook Islands",
+  "nacionalidad": "Cook Islander"
+  },
+  {
+  "nombre": " Costa Rica",
+  "nacionalidad": "Costa Rican"
+  },
+  {
+  "nombre": " Croatia",
+  "nacionalidad": "Croatian"
+  },
+  {
+  "nombre": " Cuba",
+  "nacionalidad": "Cuban"
+  },
+  {
+  "nombre": " Cyprus",
+  "nacionalidad": "Cypriot"
+  },
+  {
+  "nombre": " Curaçao",
+  "nacionalidad": "Curacian"
+  },
+  {
+  "nombre": " Czech Republic",
+  "nacionalidad": "Czech"
+  },
+  {
+  "nombre": " Denmark",
+  "nacionalidad": "Danish"
+  },
+  {
+  "nombre": " Djibouti",
+  "nacionalidad": "Djiboutian"
+  },
+  {
+  "nombre": " Dominica",
+  "nacionalidad": "Dominican"
+  },
+  {
+  "nombre": " Dominican Republic",
+  "nacionalidad": "Dominican"
+  },
+  {
+  "nombre": " Ecuador",
+  "nacionalidad": "Ecuadorian"
+  },
+  {
+  "nombre": " Egypt",
+  "nacionalidad": "Egyptian"
+  },
+  {
+  "nombre": " El Salvador",
+  "nacionalidad": "Salvadoran"
+  },
+  {
+  "nombre": " Equatorial Guinea",
+  "nacionalidad": "Equatorial Guinean"
+  },
+  {
+  "nombre": " Eritrea",
+  "nacionalidad": "Eritrean"
+  },
+  {
+  "nombre": " Estonia",
+  "nacionalidad": "Estonian"
+  },
+  {
+  "nombre": " Ethiopia",
+  "nacionalidad": "Ethiopian"
+  },
+  {
+  "nombre": " Falkland Islands (Malvinas)",
+  "nacionalidad": "Falkland Islander"
+  },
+  {
+  "nombre": " Faroe Islands",
+  "nacionalidad": "Faroese"
+  },
+  {
+  "nombre": " Fiji",
+  "nacionalidad": "Fijian"
+  },
+  {
+  "nombre": " Finland",
+  "nacionalidad": "Finnish"
+  },
+  {
+  "nombre": " France",
+  "nacionalidad": "French"
+  },
+  {
+  "nombre": " French Guiana",
+  "nacionalidad": "French Guianese"
+  },
+  {
+  "nombre": " French Polynesia",
+  "nacionalidad": "French Polynesian"
+  },
+  {
+  "nombre": " French Southern and Antarctic Lands",
+  "nacionalidad": "French"
+  },
+  {
+  "nombre": " Gabon",
+  "nacionalidad": "Gabonese"
+  },
+  {
+  "nombre": " Gambia",
+  "nacionalidad": "Gambian"
+  },
+  {
+  "nombre": " Georgia",
+  "nacionalidad": "Georgian"
+  },
+  {
+  "nombre": " Germany",
+  "nacionalidad": "German"
+  },
+  {
+  "nombre": " Ghana",
+  "nacionalidad": "Ghanaian"
+  },
+  {
+  "nombre": " Gibraltar",
+  "nacionalidad": "Gibraltar"
+  },
+  {
+  "nombre": " Guernsey",
+  "nacionalidad": "Guernsian"
+  },
+  {
+  "nombre": " Greece",
+  "nacionalidad": "Greek"
+  },
+  {
+  "nombre": " Greenland",
+  "nacionalidad": "Greenlandic"
+  },
+  {
+  "nombre": " Grenada",
+  "nacionalidad": "Grenadian"
+  },
+  {
+  "nombre": " Guadeloupe",
+  "nacionalidad": "Guadeloupe"
+  },
+  {
+  "nombre": " Guam",
+  "nacionalidad": "Guamanian"
+  },
+  {
+  "nombre": " Guatemala",
+  "nacionalidad": "Guatemalan"
+  },
+  {
+  "nombre": " Guinea",
+  "nacionalidad": "Guinean"
+  },
+  {
+  "nombre": " Guinea-Bissau",
+  "nacionalidad": "Guinea-Bissauan"
+  },
+  {
+  "nombre": " Guyana",
+  "nacionalidad": "Guyanese"
+  },
+  {
+  "nombre": " Haiti",
+  "nacionalidad": "Haitian"
+  },
+  {
+  "nombre": " Heard and Mc Donald Islands",
+  "nacionalidad": "Heard and Mc Donald Islanders"
+  },
+  {
+  "nombre": " Honduras",
+  "nacionalidad": "Honduran"
+  },
+  {
+  "nombre": " Hong Kong",
+  "nacionalidad": "Hongkongese"
+  },
+  {
+  "nombre": " Hungary",
+  "nacionalidad": "Hungarian"
+  },
+  {
+  "nombre": " Iceland",
+  "nacionalidad": "Icelandic"
+  },
+  {
+  "nombre": " India",
+  "nacionalidad": "Indian"
+  },
+  {
+  "nombre": " Isle of Man",
+  "nacionalidad": "Manx"
+  },
+  {
+  "nombre": " Indonesia",
+  "nacionalidad": "Indonesian"
+  },
+  {
+  "nombre": " Iran",
+  "nacionalidad": "Iranian"
+  },
+  {
+  "nombre": " Iraq",
+  "nacionalidad": "Iraqi"
+  },
+  {
+  "nombre": " Ireland",
+  "nacionalidad": "Irish"
+  },
+  {
+  "nombre": " Italy",
+  "nacionalidad": "Italian"
+  },
+  {
+  "nombre": " Ivory Coast",
+  "nacionalidad": "Ivory Coastian"
+  },
+  {
+  "nombre": " Jersey",
+  "nacionalidad": "Jersian"
+  },
+  {
+  "nombre": " Jamaica",
+  "nacionalidad": "Jamaican"
+  },
+  {
+  "nombre": " Japan",
+  "nacionalidad": "Japanese"
+  },
+  {
+  "nombre": " Jordan",
+  "nacionalidad": "Jordanian"
+  },
+  {
+  "nombre": " Kazakhstan",
+  "nacionalidad": "Kazakh"
+  },
+  {
+  "nombre": " Kenya",
+  "nacionalidad": "Kenyan"
+  },
+  {
+  "nombre": " Kiribati",
+  "nacionalidad": "I-Kiribati"
+  },
+  {
+  "nombre": " Korea(North Korea)",
+  "nacionalidad": "North Korean"
+  },
+  {
+  "nombre": " Korea(South Korea)",
+  "nacionalidad": "South Korean"
+  },
+  {
+  "nombre": " Kosovo",
+  "nacionalidad": "Kosovar"
+  },
+  {
+  "nombre": " Kuwait",
+  "nacionalidad": "Kuwaiti"
+  },
+  {
+  "nombre": " Kyrgyzstan",
+  "nacionalidad": "Kyrgyzstani"
+  },
+  {
+  "nombre": " Lao PDR",
+  "nacionalidad": "Laotian"
+  },
+  {
+  "nombre": " Latvia",
+  "nacionalidad": "Latvian"
+  },
+  {
+  "nombre": " Lebanon",
+  "nacionalidad": "Lebanese"
+  },
+  {
+  "nombre": " Lesotho",
+  "nacionalidad": "Basotho"
+  },
+  {
+  "nombre": " Liberia",
+  "nacionalidad": "Liberian"
+  },
+  {
+  "nombre": " Libya",
+  "nacionalidad": "Libyan"
+  },
+  {
+  "nombre": " Liechtenstein",
+  "nacionalidad": "Liechtenstein"
+  },
+  {
+  "nombre": " Lithuania",
+  "nacionalidad": "Lithuanian"
+  },
+  {
+  "nombre": " Luxembourg",
+  "nacionalidad": "Luxembourger"
+  },
+  {
+  "nombre": " Sri Lanka",
+  "nacionalidad": "Sri Lankian"
+  },
+  {
+  "nombre": " Macau",
+  "nacionalidad": "Macanese"
+  },
+  {
+  "nombre": " Macedonia",
+  "nacionalidad": "Macedonian"
+  },
+  {
+  "nombre": " Madagascar",
+  "nacionalidad": "Malagasy"
+  },
+  {
+  "nombre": " Malawi",
+  "nacionalidad": "Malawian"
+  },
+  {
+  "nombre": " Malaysia",
+  "nacionalidad": "Malaysian"
+  },
+  {
+  "nombre": " Maldives",
+  "nacionalidad": "Maldivian"
+  },
+  {
+  "nombre": " Mali",
+  "nacionalidad": "Malian"
+  },
+  {
+  "nombre": " Malta",
+  "nacionalidad": "Maltese"
+  },
+  {
+  "nombre": " Marshall Islands",
+  "nacionalidad": "Marshallese"
+  },
+  {
+  "nombre": " Martinique",
+  "nacionalidad": "Martiniquais"
+  },
+  {
+  "nombre": " Mauritania",
+  "nacionalidad": "Mauritanian"
+  },
+  {
+  "nombre": " Mauritius",
+  "nacionalidad": "Mauritian"
+  },
+  {
+  "nombre": " Mayotte",
+  "nacionalidad": "Mahoran"
+  },
+  {
+  "nombre": " Mexico",
+  "nacionalidad": "Mexican"
+  },
+  {
+  "nombre": " Micronesia",
+  "nacionalidad": "Micronesian"
+  },
+  {
+  "nombre": " Moldova",
+  "nacionalidad": "Moldovan"
+  },
+  {
+  "nombre": " Monaco",
+  "nacionalidad": "Monacan"
+  },
+  {
+  "nombre": " Mongolia",
+  "nacionalidad": "Mongolian"
+  },
+  {
+  "nombre": " Montenegro",
+  "nacionalidad": "Montenegrin"
+  },
+  {
+  "nombre": " Montserrat",
+  "nacionalidad": "Montserratian"
+  },
+  {
+  "nombre": " Morocco",
+  "nacionalidad": "Moroccan"
+  },
+  {
+  "nombre": " Mozambique",
+  "nacionalidad": "Mozambican"
+  },
+  {
+  "nombre": " Myanmar",
+  "nacionalidad": "Myanmarian"
+  },
+  {
+  "nombre": " Namibia",
+  "nacionalidad": "Namibian"
+  },
+  {
+  "nombre": " Nauru",
+  "nacionalidad": "Nauruan"
+  },
+  {
+  "nombre": " Nepal",
+  "nacionalidad": "Nepalese"
+  },
+  {
+  "nombre": " Netherlands",
+  "nacionalidad": "Dutch"
+  },
+  {
+  "nombre": " Netherlands Antilles",
+  "nacionalidad": "Dutch Antilier"
+  },
+  {
+  "nombre": " New Caledonia",
+  "nacionalidad": "New Caledonian"
+  },
+  {
+  "nombre": " New Zealand",
+  "nacionalidad": "New Zealander"
+  },
+  {
+  "nombre": " Nicaragua",
+  "nacionalidad": "Nicaraguan"
+  },
+  {
+  "nombre": " Niger",
+  "nacionalidad": "Nigerien"
+  },
+  {
+  "nombre": " Nigeria",
+  "nacionalidad": "Nigerian"
+  },
+  {
+  "nombre": " Niue",
+  "nacionalidad": "Niuean"
+  },
+  {
+  "nombre": " Norfolk Island",
+  "nacionalidad": "Norfolk Islander"
+  },
+  {
+  "nombre": " Northern Mariana Islands",
+  "nacionalidad": "Northern Marianan"
+  },
+  {
+  "nombre": " Norway",
+  "nacionalidad": "Norwegian"
+  },
+  {
+  "nombre": " Oman",
+  "nacionalidad": "Omani"
+  },
+  {
+  "nombre": " Pakistan",
+  "nacionalidad": "Pakistani"
+  },
+  {
+  "nombre": " Palau",
+  "nacionalidad": "Palauan"
+  },
+  {
+  "nombre": " Palestine",
+  "nacionalidad": "Palestinian"
+  },
+  {
+  "nombre": " Panama",
+  "nacionalidad": "Panamanian"
+  },
+  {
+  "nombre": " Papua New Guinea",
+  "nacionalidad": "Papua New Guinean"
+  },
+  {
+  "nombre": " Paraguay",
+  "nacionalidad": "Paraguayan"
+  },
+  {
+  "nombre": " Peru",
+  "nacionalidad": "Peruvian"
+  },
+  {
+  "nombre": " Philippines",
+  "nacionalidad": "Filipino"
+  },
+  {
+  "nombre": " Pitcairn",
+  "nacionalidad": "Pitcairn Islander"
+  },
+  {
+  "nombre": " Poland",
+  "nacionalidad": "Polish"
+  },
+  {
+  "nombre": " Portugal",
+  "nacionalidad": "Portuguese"
+  },
+  {
+  "nombre": " Puerto Rico",
+  "nacionalidad": "Puerto Rican"
+  },
+  {
+  "nombre": " Qatar",
+  "nacionalidad": "Qatari"
+  },
+  {
+  "nombre": " Reunion Island",
+  "nacionalidad": "Reunionese"
+  },
+  {
+  "nombre": " Romania",
+  "nacionalidad": "Romanian"
+  },
+  {
+  "nombre": " Russian",
+  "nacionalidad": "Russian"
+  },
+  {
+  "nombre": " Rwanda",
+  "nacionalidad": "Rwandan"
+  },
+  {
+  "nombre": " Saint Kitts and Nevis",
+  "nacionalidad": ""
+  },
+  {
+  "nombre": " Saint Martin (French part)",
+  "nacionalidad": "St. Martian(French)"
+  },
+  {
+  "nombre": " Sint Maarten (Dutch part)",
+  "nacionalidad": "St. Martian(Dutch)"
+  },
+  {
+  "nombre": " Saint Pierre and Miquelon",
+  "nacionalidad": "St. Pierre and Miquelon"
+  },
+  {
+  "nombre": " Saint Vincent and the Grenadines",
+  "nacionalidad": "Saint Vincent and the Grenadines"
+  },
+  {
+  "nombre": " Samoa",
+  "nacionalidad": "Samoan"
+  },
+  {
+  "nombre": " San Marino",
+  "nacionalidad": "Sammarinese"
+  },
+  {
+  "nombre": " Sao Tome and Principe",
+  "nacionalidad": "Sao Tomean"
+  },
+  {
+  "nombre": " Saudi Arabia",
+  "nacionalidad": "Saudi Arabian"
+  },
+  {
+  "nombre": " Senegal",
+  "nacionalidad": "Senegalese"
+  },
+  {
+  "nombre": " Serbia",
+  "nacionalidad": "Serbian"
+  },
+  {
+  "nombre": " Seychelles",
+  "nacionalidad": "Seychellois"
+  },
+  {
+  "nombre": " Sierra Leone",
+  "nacionalidad": "Sierra Leonean"
+  },
+  {
+  "nombre": " Singapore",
+  "nacionalidad": "Singaporean"
+  },
+  {
+  "nombre": " Slovakia",
+  "nacionalidad": "Slovak"
+  },
+  {
+  "nombre": " Slovenia",
+  "nacionalidad": "Slovenian"
+  },
+  {
+  "nombre": " Solomon Islands",
+  "nacionalidad": "Solomon Island"
+  },
+  {
+  "nombre": " Somalia",
+  "nacionalidad": "Somali"
+  },
+  {
+  "nombre": " South Africa",
+  "nacionalidad": "South African"
+  },
+  {
+  "nombre": " South Georgia and the South Sandwich",
+  "nacionalidad": "South Georgia and the South Sandwich"
+  },
+  {
+  "nombre": " South Sudan",
+  "nacionalidad": "South Sudanese"
+  },
+  {
+  "nombre": " Spain",
+  "nacionalidad": "Spanish"
+  },
+  {
+  "nombre": " Saint Helena",
+  "nacionalidad": "St. Helenian"
+  },
+  {
+  "nombre": " Sudan",
+  "nacionalidad": "Sudanese"
+  },
+  {
+  "nombre": " Suriname",
+  "nacionalidad": "Surinamese"
+  },
+  {
+  "nombre": " Svalbard and Jan Mayen",
+  "nacionalidad": "Svalbardian/Jan Mayenian"
+  },
+  {
+  "nombre": " Swaziland",
+  "nacionalidad": "Swazi"
+  },
+  {
+  "nombre": " Sweden",
+  "nacionalidad": "Swedish"
+  },
+  {
+  "nombre": " Switzerland",
+  "nacionalidad": "Swiss"
+  },
+  {
+  "nombre": " Syria",
+  "nacionalidad": "Syrian"
+  },
+  {
+  "nombre": " Taiwan",
+  "nacionalidad": "Taiwanese"
+  },
+  {
+  "nombre": " Tajikistan",
+  "nacionalidad": "Tajikistani"
+  },
+  {
+  "nombre": " Tanzania",
+  "nacionalidad": "Tanzanian"
+  },
+  {
+  "nombre": " Thailand",
+  "nacionalidad": "Thai"
+  },
+  {
+  "nombre": " Timor-Leste",
+  "nacionalidad": "Timor-Lestian"
+  },
+  {
+  "nombre": " Togo",
+  "nacionalidad": "Togolese"
+  },
+  {
+  "nombre": " Tokelau",
+  "nacionalidad": "Tokelaian"
+  },
+  {
+  "nombre": " Tonga",
+  "nacionalidad": "Tongan"
+  },
+  {
+  "nombre": " Trinidad and Tobago",
+  "nacionalidad": "Trinidadian/Tobagonian"
+  },
+  {
+  "nombre": " Tunisia",
+  "nacionalidad": "Tunisian"
+  },
+  {
+  "nombre": " Turkey",
+  "nacionalidad": "Turkish"
+  },
+  {
+  "nombre": " Turkmenistan",
+  "nacionalidad": "Turkmen"
+  },
+  {
+  "nombre": " Turks and Caicos Islands",
+  "nacionalidad": "Turks and Caicos Islands"
+  },
+  {
+  "nombre": " Tuvalu",
+  "nacionalidad": "Tuvaluan"
+  },
+  {
+  "nombre": " Uganda",
+  "nacionalidad": "Ugandan"
+  },
+  {
+  "nombre": " Ukraine",
+  "nacionalidad": "Ukrainian"
+  },
+  {
+  "nombre": " United Arab Emirates",
+  "nacionalidad": "Emirati"
+  },
+  {
+  "nombre": " United Kingdom",
+  "nacionalidad": "British"
+  },
+  {
+  "nombre": " United States",
+  "nacionalidad": "American"
+  },
+  {
+  "nombre": " US Minor Outlying Islands",
+  "nacionalidad": "US Minor Outlying Islander"
+  },
+  {
+  "nombre": " Uruguay",
+  "nacionalidad": "Uruguayan"
+  },
+  {
+  "nombre": " Uzbekistan",
+  "nacionalidad": "Uzbek"
+  },
+  {
+  "nombre": " Vanuatu",
+  "nacionalidad": "Vanuatuan"
+  },
+  {
+  "nombre": " Venezuela",
+  "nacionalidad": "Venezuelan"
+  },
+  {
+  "nombre": " Vietnam",
+  "nacionalidad": "Vietnamese"
+  },
+  {
+  "nombre": " Virgin Islands (U.S.)",
+  "nacionalidad": "American Virgin Islander"
+  },
+  {
+  "nombre": " Vatican City",
+  "nacionalidad": "Vatican"
+  },
+  {
+  "nombre": " Wallis and Futuna Islands",
+  "nacionalidad": "Wallisian/Futunan"
+  },
+  {
+  "nombre": " Western Sahara",
+  "nacionalidad": "Sahrawian"
+  },
+  {
+  "nombre": " Yemen",
+  "nacionalidad": "Yemeni"
+  },
+  {
+  "nombre": " Zambia",
+  "nacionalidad": "Zambian"
+  },
+  {
+  "nombre": " Zimbabwe",
+  "nacionalidad": "Zimbabwean"
+  }
+  ]
+)
