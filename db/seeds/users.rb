585f1ec63eeb9076a15e9a312c2f8008b238c083
hardow2011 <hardow2011@outlook.com>
2022-01-29 21:57:55 -0400

    Default admin user updated

--- db/seeds/users.rb
+++ db/seeds/users.rb
@@ -1,5 +1,5 @@
 usuario= User.new(email: "admin@admin.com", admin:true, profesor: true, password: "admin", password_confirmation: "admin")
-persona=Persona.new(nombres:"Admin", apellidos: "Admin", fecha_nacimiento: Date.new(), correo_electronico: "admin@example.com", matricula: "99999999")
+persona=Persona.new(nombres:"Admin", apellidos: "Admin", fecha_nacimiento: Date.new(), correo_electronico: "admin@admin.com", matricula: "99999999")
 persona.save!
 usuario.persona=persona
 usuario.save!
